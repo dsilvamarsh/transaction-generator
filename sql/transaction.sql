e2268474bcee073a7477e9d570840b7ab4323f7e
Marsh <dsilvamarsh@gmail.com>
2024-05-29 13:07:01 +0530

    transaction creation

---
+++ sql/transaction.sql
@@ -0,0 +1,13 @@
+CREATE  TABLE core.transaction(
+	id bigserial primary key,
+	src_acc integer,
+	tgt_acc integer,
+	transfer_amount numeric(18,3),
+	create_ts timestamp,
+	exchange_rate numeric(5,3),
+	currency_code varchar(10)
+
+) tablespace dev;
+
+alter table core.transaction
+	owner to dev;
--- transaction.py
+++ transaction.py
@@ -1,4 +1,13 @@
+import psycopg
 
+import db_config
 
 
 class TransactionRepo():
+    def save(self):
+        query = """"""
+        query_params=""
+
+        with psycopg.connect(** db_config.load_db_config()) as conn:
+            with conn.cursor() as cursor :
+                cursor.execute(query=query,params=query_params)
